2fa432a4ca4929f6d9f4245f6d1ed9796b74a010
farzeena123 <farzeena2000@gmail.com>
2023-02-25 12:01:33 +0300

    Updates

---
+++ precache-manifest.ad1da0006d60f5fbf10ad9ac92159aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04a952b5c9e9bf897699",
+    "url": "/clone/static/css/main.57dc8fd8.chunk.css"
+  },
+  {
+    "revision": "04a952b5c9e9bf897699",
+    "url": "/clone/static/js/main.04a952b5.chunk.js"
+  },
+  {
+    "revision": "569a651eee84ec0483b5",
+    "url": "/clone/static/js/1.569a651e.chunk.js"
+  },
+  {
+    "revision": "22747aba6076fb4f4afd",
+    "url": "/clone/static/js/runtime~main.22747aba.js"
+  },
+  {
+    "revision": "dc497b053f53e2d37125bf99d3fefee3",
+    "url": "/clone/index.html"
+  }
+];
